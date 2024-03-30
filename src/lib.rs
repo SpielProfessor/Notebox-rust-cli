88807d9533e58d3a9d6f4fdfef96dfdef3e01098
SpielProfessor <96953006+SpielProfessor@users.noreply.github.com>
2024-03-30 15:27:49 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 /* Spielprofessor/MrVollbart's utils
  * for the rust programming language
  * v1.0
- * This code can be used by anyone, but please keep this notice (or at least line 1, 4) at the top of your file
- * (integrated with other stuff: something like "some functions by mrvollbart)
  * 2024
- *
  */
 
 use std::io;
